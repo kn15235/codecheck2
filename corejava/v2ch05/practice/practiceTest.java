ab0934e5655ac49ca92857f250b64b8e42c5d488
Kristine Nguyen <knguyen15235@gmail.com>
2024-08-09 05:43:58 +0000

    Code for similar program TestDB

--- corejava/v2ch05/practice/practiceTest.java
+++ corejava/v2ch05/practice/practiceTest.java
@@ -16,19 +16,53 @@ public class practiceTest
         {
             runTest();
         }
-        catch (SQLExeption e)
+        catch (SQLException e)
         {
             for (Throwable t : e)
                 t.printStackTrace();
         }
     }
+
+    // making a test make the PROBLEMS table with columns REPO, KEY, and CONTENTS. 
+    // The first two are VARCHAR, the third BYTEA (the Postgres version of BLOB). Insert a couple of rows and select
+
+    public static void runTest() throws SQLException, IOException
+    {
+        try (Connection conn = getConnection();
+                Statement stat = conn.createStatement())
+        {
+            stat.executeUpdate("CREATE TABLE PROBLEM (REPO VARCHAR(10), KEY VARCHAR(10), CONTENTS BYTEA)");
+            stat.executeUpdate("INSERT INTO PROBLEM VALUES ('1', 'ABCDEFG', '\b012345')");
+            stat.executeUpdate("INSERT INTO PROBLEM VALUES ('2', 'HIJKLM', '\b012345')");
+            stat.executeUpdate("INSERT INTO PROBLEM VALUES ('3', 'NOPQRS', '\b012345')");
+            stat.executeUpdate("INSERT INTO PROBLEM VALUES ('4', 'TUVWX', '\b012345')");
+            stat.executeUpdate("INSERT INTO PROBLEM VALUES ('5', 'YZ', '\b012345')");
+
+            try (ResultSet result = stat.executeQuery("SELECT * FROM PROBLEM"))
+            {
+                if (result.next())
+                System.out.println(result.getString(1));
+            }
+            stat.executeUpdate("DROP TABLE PROBELM");
+        }
+    }
+
+    //section for the
+    public static Connection getConnection() throws SQLException, IOException
+    {
+        var props = new Properties();
+        try (Reader in = Files.newBufferedReader(
+                Path.of("database.properties"), StandardCharsets.UTF_8))
+        {
+            props.load(in);
+        }
+        String drivers = props.getProperty("jdbc.drivers");
+        if (drivers != null) System.setProperty("jdbc.drivers", drivers);
+        String url = props.getProperty("jdbc.url");
+        String username = props.getProperty("jdbc.username");
+        String password = props.getProperty("jdbc.password");
+
+        return DriverManager.getConnection(url, username, password);
+    }
 }
 
-// making a test make the PROBLEMS table with columns REPO, KEY, and CONTENTS. 
-// The first two are VARCHAR, the third BYTEA (the Postgres version of BLOB). Insert a couple of rows and select
-
-public static void runTest() throws SQLException, IOException
-{
-    try (Connection conn = getConnection();
-            Statement stat = conn.createStatement())
-}
